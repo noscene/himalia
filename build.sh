06e2724f3470246fda1dd1ad8ca8fc2db168ef7c
Sven Braun <me@no-scene.de>
2020-12-30 13:12:21 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cd && arduino-cli compile -v  --fqbn  dadamachines_doppler:samd:Himalia -o myBin /PRJ/IOCore/HimaliaSketch/
+# cd && arduino-cli compile -v  --fqbn  dadamachines_doppler:samd:Himalia -o myBin /PRJ/IOCore/HimaliaSketch/
+cd && arduino-cli compile  --output-dir  . -v  --fqbn  dadamachines_doppler:samd:Himalia  /PRJ/IOCore/HimaliaSketch/
+
 chmod 755 /root/uf2/utils/uf2conv.py
 uf2/utils/uf2conv.py -b 0x4000 -c -o mybin.uf2 myBin.bin
 cp mybin.uf2 /USB/Himalia/
